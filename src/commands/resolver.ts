39839ee7631ca0c9aeae395b48780c5698d69c1c
TehZarathustra <tehzarathustra@gmail.com>
2026-08-09 11:42:24 +0200

    Unifies stdout

--- src/commands/resolver.ts
+++ src/commands/resolver.ts
@@ -4,7 +4,7 @@ import {focus} from './focus/index.ts';
 
 const normalizeArgv = ([_, __, ...cmds]: string[]) => cmds;
 
-type Cmd = (params: string[]) => unknown;
+type Cmd = (params: string[]) => string;
 
 export const resolver = (argv: string[]) => {
   const [cmd, ...params] = normalizeArgv(argv);
@@ -17,8 +17,11 @@ export const resolver = (argv: string[]) => {
 
   const tool = cmds[cmd];
 
-  if (!tool) return console.log(`non existing tool: ${cmd}, try -h`);
-
-  // some unified stdout?
-  console.log(tool(params));
+  return tool
+    ? hexisOut(tool(params))
+    : hexisOut(`non existing tool: ${cmd}, try -h`);
 }
+
+const hexisOut = (str: string) => console.log(
+  `> hexis out: ${str}`
+);
