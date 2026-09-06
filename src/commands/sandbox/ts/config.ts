d1f551ad7ca345314c266af54b65920614a3a3eb
TehZarathustra <tehzarathustra@gmail.com>
2026-09-06 10:03:29 +0200

    sandbox:ts fixes inconstistent names

--- src/commands/sandbox/ts/config.ts
+++ src/commands/sandbox/ts/config.ts
@@ -3,7 +3,7 @@ import {resolve} from 'node:path';
 const {dirname} = import.meta;
 const root = resolve(dirname, '../../..');
 
-const PARENT_FOLDER = 'ts';
+const PARENT_DIR = 'ts';
 const START_SH = 'start_session.sh';
 const RESUME_SH = 'resume_session.sh';
 
@@ -11,7 +11,7 @@ export const getStartSH = () => resolve(dirname, START_SH);
 export const getResumeSH = () => resolve(dirname, RESUME_SH);
 
 export const getParentDir = (addon: string = '') =>
-  resolve(root, '..', 'files', PARENT_FOLDER, addon);
+  resolve(root, '..', 'files', PARENT_DIR, addon);
 
 export const getTargetFile = (filename: string) =>
   resolve(getParentDir(), `${filename}.ts`);
